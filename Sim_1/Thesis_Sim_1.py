1ddf67cca55d2a0184ae9c2e5f3453989927d2d6
Thomas Lando <tlan3750@uni.sydney.edu.au>
2022-06-25 11:10:46 +1000

    Cleanup

---
+++ Sim_1/Thesis_Sim_1.py
@@ -0,0 +1,15 @@
+# -----------------------------------------------------------------------------------------------------------------
+# Project     : Thomas Lando Thesis (Honours)
+# Title       : Simulation 1: 2D Theory
+# Author      : Thomas Lando (SID: 490388538)
+# Supervisor  : Gareth Vio
+# Institution : The University of Sydney
+# ------------------------------------------------------------------------------------------------------------------
+#
+# Description:
+# This project is focused on the use of ... for the purposes of flutter suppression in a ... inspired rocket fin.
+#
+# Nomenclature:
+#
+# Code
+#
