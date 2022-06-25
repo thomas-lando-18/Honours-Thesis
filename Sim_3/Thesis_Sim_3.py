e85441c93577d282c262bf2065bfdebf9dbeaa70
Thomas Lando <tlan3750@uni.sydney.edu.au>
2022-06-25 11:12:00 +1000

    Added 3rd Sim

---
+++ Sim_3/Thesis_Sim_3.py
@@ -0,0 +1,19 @@
+# -----------------------------------------------------------------------------------------------------------------
+# Project     : Thomas Lando Thesis (Honours)
+# Title       : Simulation 3: 3D NASTRAN
+# Author      : Thomas Lando (SID: 490388538)
+# Supervisor  : Gareth Vio
+# Institution : The University of Sydney
+# ------------------------------------------------------------------------------------------------------------------
+#
+# Description:
+# This project is focused on the use of ... for the purposes of flutter suppression in a ... inspired rocket fin.
+#
+# Nomenclature:
+#
+# Code
+# ------------------------------------------------------------------------------------------------------------------
+
+# Import Packages
+import numpy as np
+import matplotlib.pyplot as plt
