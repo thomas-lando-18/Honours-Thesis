8fd01a326c307c194ee60cdd0a41cc8445d6b4c0
Thomas Lando <tlan3750@uni.sydney.edu.au>
2022-07-03 17:01:04 +1000

    Added Trailing Edge Flap

--- Test Wing/uiuc_wing.py
+++ Test Wing/uiuc_wing.py
@@ -6,9 +6,38 @@ import matplotlib.pyplot as plt
 import numpy as np
 from write_data_to_file import write_2d_grid, write_cquad4
 
-# filename = "UIUC Data/n0011sc.dat.txt"
-filename = "UIUC Data/whitcomb.dat"
+filename = "UIUC Data/n0011sc.dat.txt"
+# filename = "UIUC Data/whitcomb.dat"
 top_surface, bottom_surface, middle_surface = extract_uiuc_data(filename)
+chord = 1
+top_surface = top_surface * chord
+middle_surface = middle_surface * chord
+bottom_surface = bottom_surface * chord
+# Add Flap Angle
+beta = np.deg2rad(0)
+# set flap point at 60% of chord
+flap_point = 0.6 * chord
+
+# Rotate values past the flap point
+rot_x = np.cos(beta)
+rot_y = np.sin(beta)
+for n in range(len(top_surface)):
+    x = top_surface[n][0]
+    y = top_surface[n][1]
+    if x > flap_point:
+        top_surface[n][:] = [rot_x*x + rot_y*y + 0.05*chord, -rot_y*x + rot_x*y + flap_point*rot_y]
+
+for n in range(len(middle_surface)):
+    x = middle_surface[n][0]
+    y = middle_surface[n][1]
+    if x > flap_point:
+        middle_surface[n][:] = [rot_x*x + rot_y*y + 0.05*chord, -rot_y*x + rot_x*y + flap_point*rot_y]
+
+for n in range(len(bottom_surface)):
+    x = bottom_surface[n][0]
+    y = bottom_surface[n][1]
+    if x > flap_point:
+        bottom_surface[n][:] = [rot_x*x + rot_y*y + 0.05*chord, -rot_y*x + rot_x*y + flap_point*rot_y]
 
 # Write Geometry Card
 fid = open('nastran_geometry_input.dat', 'w')
@@ -21,7 +50,6 @@ for n in range(len(middle_surface)):
 for n in range(len(bottom_surface)):
     write_2d_grid(fid, point_id=n + 1 + len(top_surface) + len(middle_surface), x=bottom_surface[n][0],
                   y=bottom_surface[n][1], z=0)
-
 
 # Write Nastran Mesh Card
 top_panels = np.zeros([len(middle_surface), 4])
